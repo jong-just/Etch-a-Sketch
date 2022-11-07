878679bcbcefe14996a5de41a3ca670ce7c407a7
justin h <joosteen96@gmail.com>
2022-11-06 17:04:59 -0800

    Add ability to input grid size

--- js/javascript.js
+++ js/javascript.js
@@ -2,12 +2,20 @@ const container = document.querySelector("#container");
 const columns = [];
 const resetButton = document.querySelector("#reset");
 const rows = [];
-// const gridSquare = document.createElement("div");
+const grid = [];
+let res = prompt("How big you want it?", "");
+
+for (let i = 0; i < res; i++) {
+    const pillar = document.createElement("div");
+    pillar.setAttribute("id", `column${i}`);
+
+    container.appendChild(pillar);
+}
 
 
 resetButton.addEventListener("click", () => {
-    for (let j = 0; j < 16; j++) {
-        for (let i = 0; i < 16; i++) {
+    for (let j = 0; j < res; j++) {
+        for (let i = 0; i < res; i++) {
             document.getElementById(`gridSquare${j}_${i}`).classList.remove("hover"); 
         }
     }
@@ -15,16 +23,15 @@ resetButton.addEventListener("click", () => {
 
 function createColumns() {
 
-    for (let i = 0; i < 16; i++) {
-        // eval("const column" + i + " = document.querySelector(#" + i + ")" + ";");
+    for (let i = 0; i < res; i++) {
         columns[i] = document.querySelector(`#column${i}`);
         columns[i].classList.add("grid");
     }
     return columns
 } createColumns();
 
-for (let j = 0; j < 16; j++) {
-    for (let i = 0; i < 16; i++) {
+for (let j = 0; j < res; j++) {
+    for (let i = 0; i < res; i++) {
         rows[i] = document.createElement("div");
         rows[i].setAttribute("id", `gridSquare${j}_${i}`);
         rows[i].classList.add("gridSquare");
@@ -34,8 +41,8 @@ for (let j = 0; j < 16; j++) {
 
 }
 
-for (let j = 0; j < 16; j++) {
-    for (let i = 0; i < 16; i++) {
+for (let j = 0; j < res; j++) {
+    for (let i = 0; i < res; i++) {
         document.getElementById(`gridSquare${j}_${i}`).addEventListener("pointerover", () => {
             document.getElementById(`gridSquare${j}_${i}`).classList.add("hover");
         }); 
